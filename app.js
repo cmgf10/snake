22a979075fc2567dd8edabcfe4b89f1c9406b3e1
Carlos Guilherme <cmgf10@yahoo.com.br>
2019-03-29 12:55:35 -0300

    adjusting port to heroku

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
+app.set('port',(process.env.PORT || 2000));
+
+//app.use(express.static(__dirname + '/client'));
+
+app.use('/client', express.static(__dirname + '/client'));
+
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/client/index.html');
 });
 
-app.use('/client', express.static(__dirname + '/client'));
 
+
+/*
 serv.listen(2000);
 console.log("Server started");
 
@@ -93,3 +100,4 @@ setInterval(function(){
 		}
 },2000/15);
 
+*/
